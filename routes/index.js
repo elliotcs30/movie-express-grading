0cec785a8cc13c9122c990dbe90d9a3d32f88de7
Elliot <elliotcs30@gmail.com>
2022-10-29 10:59:54 +0800

    feat: user signup page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // 新增，載入 controller
-const movieController = require('../controllers/movie-controller')
 const admin = require('./modules/admin')
+const movieController = require('../controllers/movie-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/movies', movieController.getMovies)
 
 router.use('/', (req, res) => res.redirect('/movies'))
