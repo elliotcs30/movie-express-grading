7748ef50f6f7a5ada596d806f1e2eee2c2a02ac2
Elliot <elliotcs30@gmail.com>
2022-10-29 11:15:37 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const movieController = require('../controllers/movie-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -12,5 +13,6 @@ router.post('/signup', userController.signUp)
 router.get('/movies', movieController.getMovies)
 
 router.use('/', (req, res) => res.redirect('/movies'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
