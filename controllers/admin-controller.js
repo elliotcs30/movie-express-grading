9e3c933b0be2155d87b22f08b09c9679b368970d
Elliot <elliotcs30@gmail.com>
2022-10-31 23:47:31 +0800

    feat: add movie image

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Movie } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers') // 將 file-helper 載進來
 
 const adminController = {
   getMovies: async (req, res, next) => {
@@ -14,17 +15,21 @@ const adminController = {
     return res.render('admin/create-movie')
   },
   postMovie: (req, res, next) => {
-    const { title, genres, description, releaseDate, image } = req.body
+    const { title, genres, description, releaseDate } = req.body
 
     if (!title) throw new Error('Restaurant name is required!') // title 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
 
-    Movie.create({ // 產生一個新的 Movie 物件實例，並存入資料庫
-      title,
-      genres,
-      description,
-      releaseDate,
-      image
-    })
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    localFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
+      .then(filePath => {
+        Movie.create({
+          title,
+          genres,
+          description,
+          releaseDate,
+          image: filePath || null
+        })
+      })
       .then(() => {
         req.flash('success_messages', 'movie was successfully created') // 在畫面顯示成功提示
         res.redirect('/admin/movies') // 新增完成後導回後台首頁
@@ -38,7 +43,6 @@ const adminController = {
       .then(movie => {
         // 如果找不到，回傳錯誤訊息，後面不執行
         if (!movie) throw new Error("Movie didn't exist!")
-
         res.render('admin/movie', { movie })
       })
       .catch(err => next(err))
@@ -52,21 +56,24 @@ const adminController = {
     }).catch(err => next(err))
   },
   putMovie: (req, res, next) => {
-    const { title, genres, description, releaseDate, image } = req.body
+    const { title, genres, description, releaseDate } = req.body
 
     if (!title) throw new Error('Title name is required!')
 
-    Movie.findByPk(req.params.id)
-      .then(movie => {
-        if (!movie) throw new Error('Movie name is required!')
-        return movie.update({
-          title,
-          genres,
-          description,
-          releaseDate,
-          image
-        })
+    const { file } = req // 把檔案取出來
+    Promise.all([ // 非同步處理
+      Movie.findByPk(req.params.id), // 去資料庫查有沒有這部電影
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ]).then(([movie, filePath]) => { // 以上兩樣事都做完以後
+      if (!movie) throw new Error('Movie name is required!')
+      return movie.update({
+        title,
+        genres,
+        description,
+        releaseDate,
+        image: filePath || movie.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
       })
+    })
       .then(() => {
         req.flash('success_messages', 'movie was successfully to update')
         res.redirect('/admin/movies')
