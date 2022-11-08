d99ab41d32cc24ad3ee069d60e80fbacdad338bb
elliot <elliotcs30@gmail.com>
2022-11-08 20:28:33 +0800

    feat: update seed files

--- seeders/20221108122602-movies-seed-file.js
+++ seeders/20221108122602-movies-seed-file.js
@@ -3,15 +3,8 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const categories = await queryInterface.sequelize.query('SELECT id FROM Categories;', { type: queryInterface.sequelize.QueryTypes.SELECT })
+
     await queryInterface.bulkInsert('movies',
       Array.from({ length: 50 }, () => ({
         title: faker.name.title(),
@@ -20,18 +13,13 @@ module.exports = {
         release_date: new Date(),
         Image: `https://loremflickr.com/320/240/rabbit,rabbit/?random=${Math.random() * 100}`,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete('movies', {})
   }
 }
