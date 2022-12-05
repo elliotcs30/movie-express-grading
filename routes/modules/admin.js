a4373500fbb62af8f86037a9db9de9da10a38cb0
Elliot <103249772+elliotcs30@users.noreply.github.com>
2022-12-05 23:32:01 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,6 +15,8 @@ router.post('/movies', upload.single('image'), adminController.postMovie)
 
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.use('/', (req, res) => res.redirect('/admin/movies'))
