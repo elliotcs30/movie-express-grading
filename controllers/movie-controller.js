8eac6a21d26b6ae2d8425a3dcb06748d839bfc51
Elliot <elliotcs30@gmail.com>
2022-12-07 20:22:32 +0800

    feat: add movies index page

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -1,6 +1,20 @@
+const { Movie, Category } = require('../models')
+
 const movieController = {
   getMovies: (req, res) => {
-    return res.render('movies')
+    return Movie.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(movies => {
+      const data = movies.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('movies', {
+        movies: data
+      })
+    })
   }
 }
 
